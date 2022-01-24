fc8673615a1728dee7bc14038a76d2ca6737ad70
Benjamin Grossiord <bgrossiord@legal-suite.com>
2022-01-24 09:58:39 +0100

    fix FileType usage

--- api/controllers/FileTypeController.js
+++ api/controllers/FileTypeController.js
@@ -5,8 +5,6 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const FileType = require('../models/FileType');
-
 module.exports = {
   suscribeToAll :async(req, res) =>{
     console.log('HEEERRRREEEE');
@@ -14,9 +12,9 @@ module.exports = {
       throw {badRequest: 'Only a client socket can subscribe to Louies.  But you look like an HTTP request to me.'};
     }
 
-    let specificfileType = await FileType.find();
-    User.subscribe(req, _.pluck(usersNamedLouie, 'id'));
-    return specificfileType;
+    let specificfileType = await FileType.find({id : '61ead64328e4fa2ae940f177'});
+    FileType.subscribe(req, _.pluck(specificfileType, 'id'));
+    return res.json({specificfileType});
   }
 };
 
